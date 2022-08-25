a3902f185cdaaacbf0e0a872f7bb06daa7e6b26a
Jeraldin Caliz <yera2697@hotmail.com>
2022-08-25 21:37:46 +0200

    Terminamos el crub basico de nuestra entidad empresa

--- src/main/java/com/Udea/Ciclo3/Controller/Controllerfull.java
+++ src/main/java/com/Udea/Ciclo3/Controller/Controllerfull.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -35,4 +37,29 @@ public class Controllerfull {
         }
         return "redirect:/AgregarEmpresa";
     }
-}
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id) {
+        Empresa emp=empresaService.getEmpresaById(id);
+        model.addAttribute("emp",emp);
+        return "editarEmpresa";
+    }
+    @PostMapping ("/ActualizarEmpresa")
+    public String updateEmpresa(@ModelAttribute("emp") Empresa emp){
+        if(empresaService.saveOrUpdateEmpresa(emp)){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/EditarEmpresa";
+    }
+    @GetMapping ("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id){
+        try{
+            empresaService.deleteEmpresa(id);
+        }catch (Exception e){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/VerEmpresas";
+        }
+
+
+    }
+
